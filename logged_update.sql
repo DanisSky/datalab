31601e46bd98592d38d2ec9c7e394417dff1f4ee
Danis <zinnatullin2012@inbox.ru>
2022-03-07 21:55:31 +0300

    updates

--- logged_update.sql
+++ logged_update.sql
@@ -1,5 +1,6 @@
-\set nrows :scale
-\set delta 100 +  :scale
+\set nrows 1411273
+\set delta 1 +  :nrows
+\set x random(1, nrows)
 begin;
-update logged set id = id + :delta;
+update logged set id = id + :delta where id = :x;
 end;
