ce8b9b1098525db122261f5581b136351ea7fff5
Danis <zinnatullin2012@inbox.ru>
2022-03-07 21:36:50 +0300

    updates

--- logged_update.sql
+++ logged_update.sql
@@ -1,5 +1,5 @@
 \set nrows :scale
 \set delta 100 +  :scale
 begin;
-update logged set id = id + :delta where id = :scale;
+update logged set id = id + :delta;
 end;
--- unlogged_update.sql
+++ unlogged_update.sql
@@ -1,5 +1,5 @@
 \set nrows :scale
 \set delta 100 +  :scale
 begin;
-update unlogged set id = id + :delta where id = :scale;
+update unlogged set id = id + :delta;
 end;
