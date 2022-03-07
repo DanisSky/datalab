f0b74c634e85b7a049d5d12ba4487db38ea64c4c
Danis <zinnatullin2012@inbox.ru>
2022-03-07 23:40:35 +0300

    updates

--- logged_update.sql
+++ logged_update.sql
@@ -1,5 +1,5 @@
 \set nrows :scale
 \set rid random(1, :scale)
 begin;
-update logged set id = currval('iterator_1') where id = :rid;
+update logged set id = nextval('iterator_1') where id = :rid;
 end;
